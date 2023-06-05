bbfc4d4d0197030b401fbd1639759b8fe898a936
AndreaRomano02 <andy.romano2002@gmail.com>
2023-06-05 14:05:39 +0200

    Complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log("JS OK");
+
+//# PRELIMINARI
+const expenseElement = document.getElementById("spesa");
+
+//# Creiamo la lista della spesa
+const expenseList = ["Carne", "Latte", "Uova", "Cipolle"];
+
+//# Inizializzo la variabile vuota
+let expense = "";
+
+//# Creo il ciclo con il FOR
+// for (let i = 0; i < expenseList.length; i++) {
+//   expense += " " + expenseList[i];
+// }
+
+//# Creo il ciclo con il WHILE
+let i = 0;
+while (i < expenseList.length) {
+  expense += " " + expenseList[i];
+  i++;
+}
+
+//# Stampo in pagina
+expenseElement.innerText = expense;
